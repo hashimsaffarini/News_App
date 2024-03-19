92ac69bdb3911d4afe4ee089b74c1075a6728199
hashimsaffarini <hashimsaffarini044@gmail.com>
2024-03-19 14:07:56 +0200

    Add search functionality and search page

--- lib/utils/app_constans.dart
+++ lib/utils/app_constans.dart
@@ -1,6 +1,6 @@
 class AppConstants {
   static const String baseUrl = 'https://newsapi.org';
   static const String apiKey = 'fce0158b70c24741b37dd8baf677acb8';
-  static const String topHeadlines = '/v2/top-headlines';
-  
+  static const String topHeadlines = '$baseUrl/v2/top-headlines';
+  static const String everything = '$baseUrl/v2/everything';
 }
